ca0c102ef786b0df20d1272297086538cff2b9ea
Haffi24 <haffisaefulloh@gmail.com>
2026-05-27 21:50:39 +0700

    gh pages fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Prak12_PDW_20240140181/', // <-- Tambahkan baris ini (sesuaikan dengan nama repo di GitHub)
 })
